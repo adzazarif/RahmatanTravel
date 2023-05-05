ffdd7f9a00d1e9f5b896f4e42d07d81db265fad3
adzazarif <adzazarf@gmail.com>
2023-05-06 01:54:11 +0700

    remove: private folder

---
+++ src/repository/LaporanRepository.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package repository;
+
+import entity.Pemesanan;
+import java.util.List;
+
+/**
+ *
+ * @author adzaz
+ */
+public class LaporanRepository {
+//    public List<Pemesanan> getIncomeDay(){
+//        
+//    }
+}
