70df7d84ec47b369d726551f94852d9c046647b5
adzazarif <adzazarf@gmail.com>
2023-05-08 12:55:23 +0700

    update minta kode

--- src/view/dialog/dialog_mintakode.java
+++ src/view/dialog/dialog_mintakode.java
@@ -6,7 +6,6 @@ import java.awt.Color;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
 import util.Conn;
 public class dialog_mintakode extends Dialog {
 
